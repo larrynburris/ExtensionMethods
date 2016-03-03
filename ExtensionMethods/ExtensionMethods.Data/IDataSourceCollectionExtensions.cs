519ba1dbdd8a0700539c3f52261366609b01b4dc
larrynburris <larry.n.burris@gmail.com>
2016-03-03 11:03:52 -0500

    Extending collections

---
+++ ExtensionMethods/ExtensionMethods.Data/IDataSourceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ExtensionMethods.Data
+{
+    public static class IDataSourceCollectionExtensions
+    {
+        public static IEnumerable<DataItem> GetAllItemsByCode(this IDataSource[] sources, string code)
+        {
+            var items = new List<DataItem>();
+            foreach(var source in sources)
+            {
+                items.AddRange(source.GetItemsByCode(code));
+            }
+            return items;
+        }
+
+        public static IEnumerable<DataItem> GetAllItemsByCode(this IEnumerable<IDataSource> sources, string code)
+        {
+            return sources.SelectMany(s => s.GetItemsByCode(code));
+        }
+    }
+}
---
+++ ExtensionMethods/ExtensionMethods.Tests/DataSourceCollectionExtensionTests.cs
@@ -0,0 +1,38 @@
+﻿using ExtensionMethods.Data;
+using ExtensionMethods.Data.Implementations;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExtensionMethods.Tests
+{
+    [TestFixture]
+    public class DataSourceCollectionExtensionTests
+    {
+        [Test]
+        public void Test_GetAllItemsByCode_Array()
+        {
+            var sources = new IDataSource[]
+            {
+                new SqlDataSource(),
+                new XmlDataSource(),
+                new ApiDataSource()
+            };
+            var items = sources.GetAllItemsByCode("xyz");
+            Assert.AreEqual(6, items.Count());
+        }
+
+        [Test]
+        public void Test_GetAllItemsByCode_Enumerable()
+        {
+            var sources = new List<IDataSource>
+            {
+                new SqlDataSource(),
+                new XmlDataSource(),
+                new ApiDataSource()
+            };
+            var items = sources.GetAllItemsByCode("xyz");
+            Assert.AreEqual(6, items.Count());
+        }
+    }
+}
