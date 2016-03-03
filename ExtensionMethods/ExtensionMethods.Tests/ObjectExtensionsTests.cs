6939f79f8cd64ce842151e0f57a505154d67d084
larrynburris <larry.n.burris@gmail.com>
2016-03-03 11:16:25 -0500

    Extending object and type to assist with logging

---
+++ ExtensionMethods/ExtensionMethods.Tests/ObjectExtensionsTests.cs
@@ -0,0 +1,61 @@
+﻿using ExtensionMethods.Data;
+using ExtensionMethods.Data.Implementations;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace ExtensionMethods.Tests
+{
+    [TestFixture]
+    public class ObjectExtensionsTests
+    {
+        [Test]
+        public void Test_ToJsonString()
+        {
+            var obj1 = int.MaxValue;
+            Debug.WriteLine("obj1 - " + obj1.ToJsonString());
+
+            var obj2 = new DateTime(2016, 3, 3);
+            Debug.WriteLine("obj2 - " + obj2.ToJsonString());
+
+            var obj3 = new DataItem
+            {
+                Code = "xyz",
+                Description = "123"
+            };
+            Debug.WriteLine("obj3 - " + obj3.ToJsonString());
+
+            IEnumerable<IDataSource> obj4 = new List<IDataSource>{
+                new SqlDataSource(),
+                new XmlDataSource(),
+                new ApiDataSource()
+            };
+            Debug.WriteLine("obj4 - " + obj4.ToJsonString());
+        }
+
+        [Test]
+        public void Test_GetJsonTypeDescription()
+        {
+            var obj1 = int.MaxValue;
+            Debug.WriteLine("obj1 - " + obj1.GetJsonTypeDescription());
+
+            var obj2 = new DateTime(2016, 3, 3);
+            Debug.WriteLine("obj2 - " + obj2.GetJsonTypeDescription());
+
+            var obj3 = new DataItem
+            {
+                Code = "xyz",
+                Description = "123"
+            };
+            Debug.WriteLine("obj3 - " + obj3.GetJsonTypeDescription());
+
+            IEnumerable<IDataSource> obj4 = new List<IDataSource>{
+                new SqlDataSource(),
+                new XmlDataSource(),
+                new ApiDataSource()
+            };
+            Debug.WriteLine("obj4 - " + obj4.GetJsonTypeDescription());
+        }
+    }
+}
---
+++ ExtensionMethods/ExtensionMethods/TypeExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExtensionMethods
+{
+    public static class TypeExtensions
+    {
+        public static TypeDescription GetDescription(this Type type)
+        {
+            return new TypeDescription
+            {
+                AssemblyQualifiedName = type.AssemblyQualifiedName,
+                FullName = type.FullName
+            };
+        }
+
+        public static string GetJsonTypeDescription(this object obj)
+        {
+            var description = obj.GetType().GetDescription();
+            return description.ToJsonString();
+        }
+    }
+
+    public class TypeDescription
+    {
+        public string FullName { get; set; }
+        public string AssemblyQualifiedName { get; set; }
+    }
+}
